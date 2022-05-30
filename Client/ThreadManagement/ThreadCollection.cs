65e53af344dcb7fc93477aa991da507fc16a8593
jimii88 <nguyen.tat.thanh@miyatsu.vn>
2022-05-30 16:41:40 +0700

    Thread management

---
+++ Client/ThreadManagement/ThreadCollection.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using static IotClient.ClientEvent;
+
+namespace IotClient.ThreadManagement
+{
+    public class ThreadCollection : List<Thread>
+    {
+        public ThreadCollection()
+        {
+
+        }
+
+        public void AddThread(DelegateThread delegateThread, CancellationToken cancellation)
+        {
+            this.Add(new Thread(() => delegateThread(cancellation)));
+        }
+
+        public void StartThread()
+        {
+            foreach (Thread thread in this)
+            {
+                thread.Start();
+            }
+        }
+
+        public void StopThread()
+        {
+            foreach (Thread thread in this)
+            {
+                thread.Join();
+            }
+        }
+    }
+}
