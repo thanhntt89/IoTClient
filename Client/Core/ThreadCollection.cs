65c59eb9eb6d7704e9f8f6d4c8e86cb8372b1423
jimii88 <nguyen.tat.thanh@miyatsu.vn>
2022-06-06 10:23:05 +0700

    deleted code not in used

--- Client/Core/ThreadCollection.cs
+++ Client/Core/ThreadCollection.cs
@@ -61,43 +61,12 @@ namespace IotSystem.Core
 
         public int RunningCount => this.FindAll(r => r.ThreadState == ThreadState.Running).Count;
 
-        public void KeepAliveOne()
-        {
-            if (this.RunningCount <= 1)
-                return;
-            int count = 0;
-
-            foreach (Thread thread in this)
-            {
-                if (thread.ThreadState == ThreadState.Running)
-                {
-                    if (count > 0)
-                    {
-                        thread.Abort();
-                    }
-                    count++;
-                }
-            }
-        }
-
         public void Join()
         {
             foreach (Thread thread in this)
             {
                 thread.Join();
             }
-        }
-
-        public async Task<bool> Stop()
-        {
-            await Task.Run(new Action(() =>
-            {
-                while (this.RunningCount != 0)
-                {
-                    Thread.Sleep(10);
-                }
-            }));
-            return true;
         }
     }
 }
