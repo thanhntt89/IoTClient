841879f7bd6c4f312856bf7b8ad8fde8d28c2012
jimii88 <nguyen.tat.thanh@miyatsu.vn>
2022-06-09 23:05:13 +0700

    Message dcu setup

---
+++ Client/MessageProcessing/DcuMessage/DcuMessageSetupEntity.cs
@@ -0,0 +1,20 @@
+﻿/**
+*Project name: IotSystem 
+* Created by: Nguyen Tat Thanh
+* File Name: DcuMessageSetupEntity.cs
+* Created date:2022/6/9 10:39 PM 
+* Copyright (c) by MVN Viet Nam Inc. All rights reserved
+**/
+
+namespace IotSystem.MessageProcessing.DcuMessage
+{
+    public class DcuMessageSetupEntity
+    {
+        public float TempHigh { get; set; }
+        public float TempLow { get; set; }
+        public float HumHigh { get; set; }
+        public float HumLow { get; set; }
+        public float TimeUpdate { get; set; }
+        public float TimeSample { get; set; }
+    }
+}
